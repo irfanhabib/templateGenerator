c87daed01608ecea014d2b64cf3e599c409829f1
Irfan Habib <irfan.habib@hpe.com>
2016-02-26 11:45:54 +0000

    first push

---
+++ xur.js
@@ -0,0 +1,10 @@
+/**
+ <div id="xur" class="tab-pane active">
+ <div class="well well-dark-transparent">
+ <h3 style="font-size: 24px; text-align: center; border-bottom: 2px solid #8d8c91; margin: 0px 0px 30px; padding: 5px 0px 5px;">
+ <img src="http://www.gamepur.com/files/images/2014/destiny-xur-agent-of-the-nine_1.jpg" width="100%" alt="Xur" title="Xur"></h3>
+ </div>
+ </div>
+
+ **/
+
