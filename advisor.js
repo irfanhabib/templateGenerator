f54dbc8fcb7922c582596e50d39eacdc9c08d319
Irfan Habib <irfan.habib@hpe.com>
2016-05-11 14:10:24 +0100

    use proxy

--- advisor.js
+++ advisor.js
@@ -11,7 +11,7 @@ var needleGet = Q.denodeify(needle.get);
 var advisorUrl = 'http://www.bungie.net/Platform/Destiny/Advisors/?definitions=true';
 
 module.exports.getAdvisor = function() {
-    return needleGet(advisorUrl, {headers: header}).then(function(response) {
+    return needleGet(advisorUrl, {headers: header, proxy: "http://proxy.sdc.hp.com:8080"}).then(function(response) {
         return response[0].body;
     });
 };
