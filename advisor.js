928a5965875e165baaa7447a5e7a5ae871057949
Irfan Habib <irfan.habib@hpe.com>
2016-07-19 09:52:03 +0100

    test

--- advisor.js
+++ advisor.js
@@ -10,8 +10,9 @@ var header = {
 var needleGet = Q.denodeify(needle.get);
 var advisorUrl = 'http://www.bungie.net/Platform/Destiny/Advisors/?definitions=true';
 
-module.exports.getAdvisor = function() {
-    return needleGet(advisorUrl, {headers: header, proxy: "http://proxy.sdc.hp.com:8080"}).then(function(response) {
+module.exports.getAdvisor = function () {
+    return needleGet(advisorUrl, {headers: header}).then(function (response) {
+        console.log(response.toString())
         return response[0].body;
     });
 };
